e431108beb7338cb77f7aa3d0fcb869d5b3c12e0
Leonid Kahle <leonid.kahle@epfl.ch>
2015-09-24 17:56:03 +0200

    try cp2k

--- doc/cp2k_plugin/try_cp2k.py
+++ doc/cp2k_plugin/try_cp2k.py
@@ -4,14 +4,17 @@ from aiida import load_dbenv
 load_dbenv()
 
 from aiida.orm import Code, Computer
-#~ from aiida.orm.calculation.job.cp2k import FloatWithUnit, Dictlist
-#~ code = Code.get_from_string(codename)
-#~ code = Code.get_subclass_from_pk(130)
-#~ computer = Computer.get('theospc25')
+from aiida.orm import DataFactory
+
+
+# Put the name of the computer you want to run
 computer = Computer.get('thea')
+
+# Put the code, cp2k, which uses the plugin cp2k.CP2KCalculation
 code =  Code.get('cp2k')
 
-from aiida.orm import DataFactory
+
+#Let's define a simple cubic structure, e.g BaTO3
 StructureData = DataFactory('structure')
 alat = 4. # angstrom
 cell = [[alat, 0., 0.,],
@@ -20,13 +23,14 @@ cell = [[alat, 0., 0.,],
        ]
 s = StructureData(cell=cell)
 s.append_atom(position=(0.,0.,0.),symbols='Ba')
-s.append_atom(position=(alat/1.9,alat/2.,alat/2.),symbols='Ti')
-s.append_atom(position=(alat/1.9,alat/2.,0.),symbols='O')
+s.append_atom(position=(alat/2.0,alat/2.,alat/2.),symbols='Ti')
+s.append_atom(position=(alat/2.0,alat/2.,0.),symbols='O')
 s.append_atom(position=(alat/2.,0.,alat/2.),symbols='O')
 s.append_atom(position=(0.,alat/2.,alat/2.),symbols='O')
 
 
 
+#Let's define some parameters
 ParameterData = DataFactory('parameter')
 
 parameters = ParameterData(dict={
@@ -76,18 +80,20 @@ parameters = ParameterData(dict={
                     },
               })
 
+
+#what about k-points?
 #~ KpointsData = DataFactory('array.kpoints')
 #~ kpoints = KpointsData()
 #~ kpoints.set_kpoints_mesh([5,5,5])
 #~ kpoints.set_kpoints_mesh([5,5,5],offset=(0.5,0.5,0.5))
 
 
+#Set up the calculation:
 calc = code.new_calc()
 calc.set_computer(computer)
 calc.set_max_wallclock_seconds(30*60) # 30 min
 calc.set_resources({"num_machines": 1, "num_mpiprocs_per_machine": 1})
 
-# calc.set_queue_name("the_queue_name")
 calc.use_structure(s)
 calc.use_code(code)
 calc.use_parameters(parameters)
@@ -97,7 +103,9 @@ calc.use_parameters(parameters)
 
 calc.label = "My first calculation"
 #~ calc.store_all()
+#I just want a submit-test
 subfolder, script_filename = calc.submit_test()
+
 print "Test submit file in {}".format(os.path.join(
             os.path.relpath(subfolder.abspath),
             script_filename

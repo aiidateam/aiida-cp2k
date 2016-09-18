474305ece4549ef5bff8b7ac2ece6a010b0fff0a
yakutovich <yakutovicha@gmail.com>
2016-09-18 20:31:09 +0200

    Initial commit

--- doc/cp2k_plugin/try_parser.py
+++
@@ -1,9 +0,0 @@
-from aiida.parsers.plugins.cp2k import  CP2KBasicParser
-
-
-
-
-calc = Calculation.get_subclass_from_pk(90)
-parser = CP2KBasicParser(calc)
-
-parser.parse_with_retrieved({'retrieved':calc.get_retrieved_node()})

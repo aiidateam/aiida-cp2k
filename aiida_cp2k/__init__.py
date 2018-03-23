0806dc41a7a8c9a24408377226c6d437059127bb
Ole Schuett <oleschuett@google.com>
2018-03-23 20:27:39 +0100

    v0.8.1

--- aiida_cp2k/__init__.py
+++ aiida_cp2k/__init__.py
@@ -8,6 +8,6 @@
 # For further information please visit http://www.aiida.net               #
 ###########################################################################
 
-__version__ = "0.8"
+__version__ = "0.8.1"
 
 # EOF

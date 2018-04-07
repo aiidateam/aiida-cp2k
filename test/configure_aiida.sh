b0114d82ed1b5cd6f8ca697f4447449a2b65442b
Ole Schuett <oleschuett@google.com>
2018-04-07 21:05:49 +0200

    Add SPDX license identifier

--- test/configure_aiida.sh
+++ test/configure_aiida.sh
@@ -1,4 +1,10 @@
 #!/bin/bash -e
+###############################################################################
+# Copyright (c), The AiiDA-CP2K authors.                                      #
+# SPDX-License-Identifier: MIT                                                #
+# AiiDA-CP2K is hosted on GitHub at https://github.com/cp2k/aiida-cp2k        #
+# For further information on the license, see the LICENSE.txt file.           #
+###############################################################################
 
 set -x
 

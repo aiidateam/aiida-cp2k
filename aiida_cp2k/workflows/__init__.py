a774bbc2c1dfbe0fdda3a6e1d21ea8e01d1d0073
gloriacapano <44054069+gloriacapano@users.noreply.github.com>
2018-10-28 14:25:43 +0100

    Bandstructure workflow initial version
    * Bandstructure workflow initial version
    
    * Modify 20% added mos, remove added mos in print

--- aiida_cp2k/workflows/__init__.py
+++ aiida_cp2k/workflows/__init__.py
@@ -3,3 +3,4 @@ from .dftgeoopt import Cp2kGeoOptWorkChain
 from .dftcellopt import Cp2kCellOptWorkChain
 from .dftmd import Cp2kMdWorkChain
 from .dftrobustgeoopt import Cp2kRobustGeoOptWorkChain
+from .dftbandstructure import Cp2kBandStructureWorkChain

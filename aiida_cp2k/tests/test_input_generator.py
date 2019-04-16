0acf6db0deee6444c812ffdb313c9d634fdac04b
Tiziano Müller <tiziano.mueller@chem.uzh.ch>
2019-04-16 10:08:33 +0200

    calculations: factor out Cp2kInput for independent testing

---
+++ aiida_cp2k/tests/test_input_generator.py
@@ -0,0 +1,45 @@
+# -*- coding: utf-8 -*-
+###############################################################################
+# Copyright (c), The AiiDA-CP2K authors.                                      #
+# SPDX-License-Identifier: MIT                                                #
+# AiiDA-CP2K is hosted on GitHub at https://github.com/cp2k/aiida-cp2k        #
+# For further information on the license, see the LICENSE.txt file.           #
+###############################################################################
+
+from __future__ import absolute_import
+from aiida_cp2k.utils import Cp2kInput
+
+
+def test_render_empty():
+    inp = Cp2kInput()
+    assert inp.render() == inp.DISCLAIMER
+
+
+def test_render_str_val():
+    inp = Cp2kInput({"FOO": "bar"})
+    assert inp.render() == "{inp.DISCLAIMER}\nFOO  bar".format(inp=inp)
+
+
+def test_add_keyword():
+    inp = Cp2kInput({"FOO": "bar"})
+    inp.add_keyword("BAR", "boo")
+    assert inp.render() == "{inp.DISCLAIMER}\nBAR  boo\nFOO  bar".format(inp=inp)
+
+
+def test_multiple_force_eval():
+    inp = Cp2kInput({"FORCE_EVAL": [{"FOO": "bar"}, {"FOO": "bar"}, {"FOO": "bar"}]})
+    assert (
+        inp.render()
+        == """{inp.DISCLAIMER}
+&FORCE_EVAL
+   FOO  bar
+&END FORCE_EVAL
+&FORCE_EVAL
+   FOO  bar
+&END FORCE_EVAL
+&FORCE_EVAL
+   FOO  bar
+&END FORCE_EVAL""".format(
+            inp=inp
+        )
+    )

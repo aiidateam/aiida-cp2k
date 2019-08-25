3ca4c00f2b4893b0ed198ed116d066f82c8b6772
Daniele Ongari <daniele.ongari@gmail.com>
2019-08-25 12:06:51 +0200

    Updated tests (now using click)

--- test/test_workchain/test_multistage_Al.py
+++ test/test_workchain/test_multistage_Al.py
@@ -1,83 +1,68 @@
 # -*- coding: utf-8 -*-
 # pylint: disable=invalid-name
-###############################################################################
-# Copyright (c), The AiiDA-CP2K authors.                                      #
-# SPDX-License-Identifier: MIT                                                #
-# AiiDA-CP2K is hosted on GitHub at https://github.com/aiidateam/aiida-cp2k   #
-# For further information on the license, see the LICENSE.txt file.           #
-###############################################################################
+""" Test/example for the Cp2kMultistageWorkChain"""
 
 from __future__ import print_function
 from __future__ import absolute_import
 
-import sys
+import click
 import ase.build
 
 from aiida.engine import run
-from aiida.orm import (Code, Dict, StructureData)
-from aiida.common import NotExistent
+from aiida.orm import Code, Dict, StructureData, Float, Str
 from aiida_cp2k.workchains import Cp2kMultistageWorkChain
 
-# =============================================================================
-if len(sys.argv) != 2:
-    print("Usage: test.py <code_name>")
-    sys.exit(1)
+@click.command('cli')
+@click.argument('cp2k_code_string')
+def main(cp2k_code_string):
+    """Example usage: verdi run cp2k-5.1@localhost"""
 
-codename = sys.argv[1]
-try:
-    code = Code.get_from_string(codename)
-except NotExistent:
-    print("The code '{}' does not exist".format(codename))
-    sys.exit(1)
+    print("Testing CP2K multistage workchain on Al (RKS, needs smearing)...")
+    print("EXPECTED: the OT (settings_0) will converge to a negative bandgap, then we switch to SMEARING (settings_1)")
 
-print("Testing CP2K multistage workchain on Al (RKS, needs smearing)...")
-print("EXPECTED: the OT (settings_0) will converge to a negative bandgap, then we switch to SMEARING (settings_1)")
+    code = Code.get_from_string(cp2k_code_string)
 
-# structure
-structure = StructureData(ase=ase.io.read('../data/Al.cif'))
+    structure = StructureData(ase=ase.io.read('../data/Al.cif'))
 
-# lowering the settings for acheaper calculation
-parameters = Dict(dict={
-        'FORCE_EVAL': {
-          'DFT': {
-            'MGRID': {
-              'CUTOFF': 280,
-              'REL_CUTOFF': 30,
-}}}})
-
-protocol_mod = Dict(dict= {
-    'initial_magnetization': {
-        'Al': 0
+    # testing user change of parameters and protocol
+    parameters = Dict(dict={
+            'FORCE_EVAL': {
+              'DFT': {
+                'MGRID': {
+                  'CUTOFF': 250,
+    }}}})
+    protocol_mod = Dict(dict= {
+        'initial_magnetization': {
+            'Al': 0
+            },
+        'settings_0': {
+            'FORCE_EVAL': {
+                'DFT': {
+                    'SCF': {
+                        'OUTER_SCF': {
+                            'MAX_SCF': 5,
+    }}}}}})
+    options = {
+        "resources": {
+            "num_machines": 1,
+            "num_mpiprocs_per_machine": 1,
         },
-    'settings_0': {
-        'FORCE_EVAL': {
-            'DFT': {
-                'SCF': {
-                    'OUTER_SCF': {
-                        'MAX_SCF': 5,
-}}}}}})
-
-options = {
-    "resources": {
-        "num_machines": 1,
-        "num_mpiprocs_per_machine": 1,
-    },
-    "max_wallclock_seconds": 1 * 3 * 60,
-}
-inputs = {
-    'protocol_tag': Str('test'),
-    'starting_settings_idx': Int(0),
-    'protocol_modify': protocol_mod,
-    'base': {
-        'cp2k': {
-            'structure': structure,
-            'parameters': parameters,
-            'code': code,
-            'metadata': {
-                'options': options,
-            }
-        }
+        "max_wallclock_seconds": 1 * 3 * 60,
     }
-}
+    inputs = {
+        'protocol_tag': Str('test'),
+        'starting_settings_idx': Int(0),
+        'protocol_modify': protocol_mod,
+        'cp2k_base': {
+            'cp2k': {
+                'structure': structure,
+                'parameters': parameters,
+                'code': code,
+                'metadata': {
+                    'options': options,
+    }}}}
 
-run(Cp2kMultistageWorkChain, **inputs)
+    run(Cp2kMultistageWorkChain, **inputs)
+
+if __name__ == '__main__':
+    main()
--- test/test_workchain/test_multistage_h2o-.py
+++ test/test_workchain/test_multistage_h2o-.py
@@ -1,72 +1,56 @@
 # -*- coding: utf-8 -*-
 # pylint: disable=invalid-name
-###############################################################################
-# Copyright (c), The AiiDA-CP2K authors.                                      #
-# SPDX-License-Identifier: MIT                                                #
-# AiiDA-CP2K is hosted on GitHub at https://github.com/aiidateam/aiida-cp2k   #
-# For further information on the license, see the LICENSE.txt file.           #
-###############################################################################
+""" Test/example for the Cp2kMultistageWorkChain"""
 
 from __future__ import print_function
 from __future__ import absolute_import
 
-import sys
+import click
 import ase.build
 
 from aiida.engine import run
-from aiida.orm import (Code, Dict, StructureData)
-from aiida.common import NotExistent
+from aiida.orm import Code, Dict, StructureData, Float, Str
 from aiida_cp2k.workchains import Cp2kMultistageWorkChain
 
-# =============================================================================
-if len(sys.argv) != 2:
-    print("Usage: test.py <code_name>")
-    sys.exit(1)
+@click.command('cli')
+@click.argument('cp2k_code_string')
+def main(cp2k_code_string):
+    """Example usage: verdi run cp2k-5.1@localhost"""
 
-codename = sys.argv[1]
-try:
-    code = Code.get_from_string(codename)
-except NotExistent:
-    print("The code '{}' does not exist".format(codename))
-    sys.exit(1)
+    print("Testing CP2K multistage workchain on H2O- (UKS, no need for smearing)...")
 
-print("Testing CP2K multistage workchain on H2O- (UKS, no need for smearing)...")
+    code = Code.get_from_string(cp2k_code_string)
 
-# structure
-atoms = ase.build.molecule('H2O')
-atoms.center(vacuum=2.0)
-structure = StructureData(ase=atoms)
+    atoms = ase.build.molecule('H2O')
+    atoms.center(vacuum=2.0)
+    structure = StructureData(ase=atoms)
 
-# lowering the settings for acheaper calculation
-parameters = Dict(dict={
-        'FORCE_EVAL': {
-          'DFT': {
-            'UKS': True,
-            'MULTIPLICITY': 2,
-            'CHARGE': -1,
-            'MGRID': {
-              'CUTOFF': 280,
-              'REL_CUTOFF': 30,
-}}}})
-options = {
-    "resources": {
-        "num_machines": 1,
-        "num_mpiprocs_per_machine": 1,
-    },
-    "max_wallclock_seconds": 1 * 3 * 60,
-}
-inputs = {
-    'protocol_tag': Str('test'),
-    'base': {
-        'cp2k': {
-            'structure': structure,
-            'parameters': parameters,
-            'code': code,
-            'metadata': {
-                'options': options,
-            }
-        }
+    parameters = Dict(dict={
+            'FORCE_EVAL': {
+              'DFT': {
+                'UKS': True,
+                'MULTIPLICITY': 2,
+                'CHARGE': -1,
+    }}})
+    options = {
+        "resources": {
+            "num_machines": 1,
+            "num_mpiprocs_per_machine": 1,
+        },
+        "max_wallclock_seconds": 1 * 3 * 60,
     }
-}
+    inputs = {
+        'protocol_tag': Str('test'),
+        'cp2k_base': {
+            'cp2k': {
+                'structure': structure,
+                'parameters': parameters,
+                'code': code,
+                'metadata': {
+                    'options': options,
+    }}}}
 
-run(Cp2kMultistageWorkChain, **inputs)
+    run(Cp2kMultistageWorkChain, **inputs)
+
+if __name__ == '__main__':
+    main()

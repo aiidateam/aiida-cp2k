68aece704f3fe68bb024d16bffd9c45c3409852a
Pezhman Zarabadi-Poor <pezhman.zarabadi@ceitec.muni.cz>
2019-10-10 20:32:07 +0200

    fix an issue with writing coordinates (#52)
    If the X coordinate of an atom contains a negative sign and two digits before the decimal point,
    it would cause the X coordinate to be glued to the symbol like C4-17.2206645768000008

--- aiida_cp2k/calculations/__init__.py
+++ aiida_cp2k/calculations/__init__.py
@@ -164,7 +164,7 @@ class Cp2kCalculation(CalcJob):
         s_ase = structure.get_ase()
         elem_tags = ['' if t == 0 else str(t) for t in s_ase.get_tags()]
         elem_symbols = list(map(add, s_ase.get_chemical_symbols(), elem_tags))
-        elem_coords = ['{:20.16f} {:20.16f} {:20.16f}'.format(p[0], p[1], p[2]) for p in s_ase.get_positions()]
+        elem_coords = ['{:25.16f} {:25.16f} {:25.16f}'.format(p[0], p[1], p[2]) for p in s_ase.get_positions()]
         with io.open(folder.get_abs_path(name), mode="w", encoding="utf-8") as fobj:
             fobj.write(u'{}\n\n'.format(len(elem_coords)))
             fobj.write(u'\n'.join(map(add, elem_symbols, elem_coords)))

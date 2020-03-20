96fb9fd4d65dab44111f25595fb87797e7707974
Aliaksandr Yakutovich <yakutovicha@gmail.com>
2020-03-20 08:55:58 +0100

    Travis and Docker file updates (#83)
    * Place the plugin in /opt/aiida-cp2k folder.
    
    * Install plugin as the root user.
    
    * Adapt .travis.yml file for those changes.

--- .docker/my_init.d/add-codes.sh
+++ .docker/my_init.d/add-codes.sh
@@ -2,3 +2,6 @@
 set -em
 
 su -c /opt/add-codes.sh aiida
+
+# Make /opt/aiida-cp2k folder editable for the $SYSTEM_USER.
+chown -R aiida:aiida /opt/aiida-cp2k/
--- .docker/opt/add-codes.sh
+++ .docker/opt/add-codes.sh
@@ -6,7 +6,4 @@ set -x
 # Environment
 export SHELL=/bin/bash
 
-# Install the ddec and cp2k codes
-CP2K_FOLDER=/home/aiida/code/aiida-cp2k
-
-verdi code show cp2k@localhost || verdi code setup --config ${CP2K_FOLDER}/.docker/cp2k-code.yml --non-interactive
+verdi code show cp2k@localhost || verdi code setup --config /opt/aiida-cp2k/.docker/cp2k-code.yml --non-interactive

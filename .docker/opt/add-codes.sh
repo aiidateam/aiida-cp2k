5e162cebc0acd15361cd301f1b3e6ec5f46a6f6b
Aliaksandr Yakutovich <yakutovicha@gmail.com>
2020-03-31 22:54:19 +0200

    Make add_codes.sh run after aiida setup.

--- .docker/opt/add-codes.sh
+++ .docker/opt/add-codes.sh
@@ -6,18 +6,5 @@ set -x
 # Environment
 export SHELL=/bin/bash
 
-# Activate conda.
-__conda_setup="$('/opt/conda/bin/conda' 'shell.bash' 'hook' 2> /dev/null)"
-if [ $? -eq 0 ]; then
-    eval "$__conda_setup"
-else
-    if [ -f "/opt/conda/etc/profile.d/conda.sh" ]; then
-        . "/opt/conda/etc/profile.d/conda.sh"
-    else
-        export PATH="/opt/conda/bin:$PATH"
-    fi
-fi
-unset __conda_setup
-
 # Install cp2k code.
 verdi code show cp2k@localhost || verdi code setup --config /opt/aiida-cp2k/.docker/cp2k-code.yml --non-interactive

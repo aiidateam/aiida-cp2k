1b91d137f7afd06b29a6a69e0ae8e1638aeac60d
Tiziano Müller <tiziano.mueller@chem.uzh.ch>
2020-09-04 15:14:36 +0200

    examples: bailout gracefully if gdt package is not available

--- conftest.py
+++ conftest.py
@@ -15,3 +15,15 @@ def clear_database(aiida_profile):
     """Clear the database after a test with this fixture"""
     yield
     aiida_profile.reset_db()
+
+
+# from https://stackoverflow.com/a/25188424
+# required for examples for optional features to show appropriate error messages
+def pytest_configure(config):  # pylint: disable=unused-argument
+    import sys
+    sys._called_from_test = True  # pylint: disable=protected-access
+
+
+def pytest_unconfigure(config):  # pylint: disable=unused-argument
+    import sys
+    del sys._called_from_test  # pylint: disable=protected-access

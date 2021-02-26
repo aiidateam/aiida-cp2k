716921f2b1ed935b2e76bf2ec2eae9d5e1e6591a
Aliaksandr Yakutovich <yakutovicha@gmail.com>
2021-02-26 17:07:33 +0100

    fix comments

--- test/test_parser.py
+++ test/test_parser.py
@@ -2,7 +2,7 @@
 ###############################################################################
 # Copyright (c), The AiiDA-CP2K authors.                                      #
 # SPDX-License-Identifier: MIT                                                #
-# AiiDA-CP2K is hosted on GitHub at https://github.com/aiidateam/aiida-cp2k        #
+# AiiDA-CP2K is hosted on GitHub at https://github.com/aiidateam/aiida-cp2k   #
 # For further information on the license, see the LICENSE.txt file.           #
 ###############################################################################
 """Test output parser."""

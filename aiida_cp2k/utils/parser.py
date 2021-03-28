ec24a4d0d0bfcaf69b789822f2574672a0bb9031
Holger-Dietrich Saßnick <holger-dietrich.sassnick@uni-oldenburg.de>
2021-03-28 15:36:23 +0200

    Added pbc to parse_cp2k_trajectory

--- aiida_cp2k/utils/parser.py
+++ aiida_cp2k/utils/parser.py
@@ -391,4 +391,8 @@ def parse_cp2k_trajectory(content):
     cell_str = [line[1:] for line in cell_lines if line[0] in "ABC"]
     cell = np.array(cell_str, np.float64)
 
-    return {"symbols": symbols, "positions": positions, "cell": cell, "tags": tags}
+    # parse periodic boundary conditions
+    cell_pbc_str = next((line[-1] for line in cell_lines if line[0] == 'PERIODIC'), None)
+    cell_pbc = [(dir in cell_pbc_str) for dir in ['X', 'Y', 'Z']]
+
+    return {"symbols": symbols, "positions": positions, "cell": cell, "tags": tags, "pbc": cell_pbc}

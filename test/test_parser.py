90915725251b2b86d33032963f2e62d9400cb11f
AndresOrtegaGuerrero <oandresg15@gmail.com>
2022-11-21 17:43:30 +0100

    post pre-commit

--- test/test_parser.py
+++ test/test_parser.py
@@ -7,7 +7,13 @@
 """Test output parser."""
 import os
 
-from aiida_cp2k.utils.parser import _parse_bands, parse_cp2k_trajectory, parse_cp2k_output, parse_cp2k_output_advanced
+from aiida_cp2k.utils.parser import (
+    _parse_bands,
+    parse_cp2k_output,
+    parse_cp2k_output_advanced,
+    parse_cp2k_trajectory,
+)
+
 THISDIR = os.path.dirname(os.path.realpath(__file__))
 
 
@@ -58,36 +64,33 @@ def test_bands_parser_81():
             bands[0] == [-6.84282475, 5.23143741, 5.23143741, 5.23143741, 7.89232311]
         ).all()
 
+
 def test_cp2k_output():
-    """Test parse_cp2k_out """
+    """Test parse_cp2k_out"""
     with open(f"{THISDIR}/outputs/cdft_dos_cp2k_6.0.out") as fobj:
         lines = fobj.readlines()
         result_dict = parse_cp2k_output(lines)
         assert result_dict["energy"] == -1544.475602321840825
         assert result_dict["energy_units"] == "a.u."
-        assert result_dict["nwarnings"]== 1
+        assert result_dict["nwarnings"] == 1
+
 
 def test_cp2k_output_advanced():
-    """Test parse_cp2k_advanced output """
+    """Test parse_cp2k_advanced output"""
     with open(f"{THISDIR}/outputs/cdft_dos_cp2k_6.0.out") as fobj:
         lines = fobj.readlines()
         result_dict = parse_cp2k_output_advanced(lines)
         assert result_dict["cp2k_version"] == 6.0
         assert result_dict["energy_scf"] == -1544.47560232184082
-        assert result_dict["nwarnings"]== 1
-        assert result_dict["run_type"]== "ENERGY"
-        assert result_dict["dft_type"]== "UKS"
+        assert result_dict["nwarnings"] == 1
+        assert result_dict["run_type"] == "ENERGY"
+        assert result_dict["dft_type"] == "UKS"
         assert result_dict["integrated_abs_spin_dens"] == 6.4548954029
-        assert result_dict["spin_square_ideal"] ==  0.750000
+        assert result_dict["spin_square_ideal"] == 0.750000
         assert result_dict["spin_square_expectation"] == 2.827411
-        assert result_dict["init_nel_spin1"] == 358 
+        assert result_dict["init_nel_spin1"] == 358
         assert result_dict["init_nel_spin2"] == 357
         assert result_dict["natoms"] == 194
-
-        
-           
-          
-
 
 
 def test_trajectory_parser_pbc():

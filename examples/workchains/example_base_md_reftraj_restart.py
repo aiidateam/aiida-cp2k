aa0510347766d0cf2230f09676a8ab03d7bff737
Carlo Antonio Pignedoli <c.pignedoli@gmail.com>
2024-03-06 12:51:45 +0000

    removed unused walltime

--- examples/workchains/example_base_md_reftraj_restart.py
+++ examples/workchains/example_base_md_reftraj_restart.py
@@ -165,7 +165,6 @@ def example_base(cp2k_code):
         "num_machines": 1,
         "num_mpiprocs_per_machine": 1,
     }
-    builder.cp2k.metadata.options.max_wallclock_seconds = 1 * 3 * 60
 
     print("Submitted calculation...")
     calc = run(builder)

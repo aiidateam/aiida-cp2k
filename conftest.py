e658affe86daa28ce33e0b8d03515fcf0c41cd82
Aliaksandr Yakutovich <yakutovicha@gmail.com>
2024-03-19 18:06:19 +0500

    Add prepend text to the cp2k_code fixture.

--- conftest.py
+++ conftest.py
@@ -7,7 +7,7 @@ pytest_plugins = ["aiida.manage.tests.pytest_fixtures"]
 @pytest.fixture(scope="function")
 def cp2k_code(aiida_local_code_factory):
     return aiida_local_code_factory(
-        entry_point="cp2k", executable="/opt/conda/envs/cp2k/bin/cp2k.psmp"
+        entry_point="cp2k", executable="/opt/conda/envs/cp2k/bin/cp2k.psmp", prepend_text='eval "$(command conda shell.bash hook 2> /dev/null)"\nconda activate cp2k\n'
     )
 
 

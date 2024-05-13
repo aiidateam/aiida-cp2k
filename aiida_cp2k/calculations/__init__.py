d4901e6df243d85e5deb0a53baf336ee2295625e
superstar54 <xingwang1991@gmail.com>
2024-05-13 16:56:47 +0200

    fix pre-commit

--- aiida_cp2k/calculations/__init__.py
+++ aiida_cp2k/calculations/__init__.py
@@ -481,7 +481,7 @@ def _trajectory_to_xyz_and_cell(trajectory):
     if "cells" in trajectory.get_arraynames():
         cell = "#   Step   Time [fs]       Ax [Angstrom]       Ay [Angstrom]       Az [Angstrom]       Bx [Angstrom]       By [Angstrom]       Bz [Angstrom]       Cx [Angstrom]       Cy [Angstrom]       Cz [Angstrom]      Volume [Angstrom^3]\n"
         cell_vecs = [
-            f"{stepid+1} {(stepid+1)*0.5:6.3f} {cellvec[0][0]:25.16f} {cellvec[0][1]:25.16f} {cellvec[0][2]:25.16f} {cellvec[1][0]:25.16f} {cellvec[1][1]:25.16f} {cellvec[1][2]:25.16f} {cellvec[2][0]:25.16f} {cellvec[2][1]:25.16f} {cellvec[2][2]:25.16f} {np.dot(cellvec[0],np.cross(cellvec[1],cellvec[2]))}"
+            f"{stepid+1} {(stepid+1)*0.5:6.3f} {cellvec[0][0]:25.16f} {cellvec[0][1]:25.16f} {cellvec[0][2]:25.16f} {cellvec[1][0]:25.16f} {cellvec[1][1]:25.16f} {cellvec[1][2]:25.16f} {cellvec[2][0]:25.16f} {cellvec[2][1]:25.16f} {cellvec[2][2]:25.16f} {np.dot(cellvec[0], np.cross(cellvec[1], cellvec[2]))}"
             for (stepid, cellvec) in zip(stepids, trajectory.get_array("cells"))
         ]
         cell += "\n".join(cell_vecs)

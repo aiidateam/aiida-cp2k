108663117ec4e96478add29bc51c98dcc304a3be
Aliaksandr Yakutovich <yakutovicha@gmail.com>
2025-02-04 11:58:27 +0100

    Update sssp installation fixture: run it only once(#228)

--- conftest.py
+++ conftest.py
@@ -16,11 +16,6 @@ def cp2k_code(aiida_local_code_factory):
     )
 
 
-@pytest.fixture(scope="function", autouse=True)
-def clear_database(aiida_profile_clean):
-    """Automatically clear database in between tests."""
-
-
 # from https://stackoverflow.com/a/25188424
 # required for examples for optional features to show appropriate error messages
 def pytest_configure(config):
@@ -35,7 +30,7 @@ def pytest_unconfigure(config):
     del sys._called_from_test
 
 
-@pytest.fixture(scope="function", autouse=True)
+@pytest.fixture(scope="session", autouse=True)
 def setup_sssp_pseudos(aiida_profile):
     """Create an SSSP pseudo potential family from scratch."""
     subprocess.run(

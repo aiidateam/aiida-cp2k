7db5d3a7742ff4e472be878eb934156290574631
Carlo Antonio Pignedoli <c.pignedoli@gmail.com>
2026-08-11 08:10:45 +0000

    Modernize CI infrastructure

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -45,8 +45,10 @@ extensions = [
 ]
 
 intersphinx_mapping = {
-    "python": ("https://docs.python.org/2.7", None),
-    "aiida": ("http://aiida-core.readthedocs.io/en/latest/", None),
+    "python": ("https://docs.python.org/3", None),
+    # AiiDA 2.7.1 is the latest 2.x inventory that publishes the class targets
+    # required by ``show-inheritance``. Newer inventories omit these API targets.
+    "aiida": ("https://aiida.readthedocs.io/projects/aiida-core/en/v2.7.1/", None),
 }
 
 # Add any paths that contain templates here, relative to this directory.
